de734facd5d0ac6d35d1c3d33e395c941503cc38
Bowarc <yuyeaaaahhhh@laposte.net>
2024-06-01 12:45:49 +0200

    Fixed a bug where format would round up units
    Added a bit of documentation
    Remove new_custom_timeline from DTDelay to add restart_custom_timeline

--- time/src/lib.rs
+++ time/src/lib.rs
@@ -1,65 +1,138 @@
-#[derive(PartialEq, Eq)]
-pub enum DelayState<T> {
-    // Timeline: --------------------------------
-    //       delay ended here|    |but `ended()` is called here
-    // We return the time between the two bars
-    Done(T), //Time since done
-    Running,
-}
+/// A delay for game animations (e.g., animation frames).
+///
+/// # Examples
+///
+/// ```
+/// use time::DTDelay;
+///
+/// let mut delay = DTDelay::new(5.0);
+/// println!("{:?}", delay); // DTDelay { starting_timeout_s: 5.0, timeout_s: 5.0 }
+///
+/// delay.update(1.0);
+/// println!("{:?}", delay); // DTDelay { starting_timeout_s: 5.0, timeout_s: 4.0 }
+///
+/// println!("Fraction: {}", delay.fraction()); // Fraction: 0.8
+///
+/// println!("Ended: {}", delay.ended()); // Ended: false
+///
+/// delay.update(4.0);
+/// println!("{:?}", delay); // DTDelay { starting_timeout_s: 5.0, timeout_s: 0.0 }
+///
+/// println!("Ended: {}", delay.ended()); // Ended: true
+///
+/// delay.update(1.0);
+/// println!("{:?}", delay); // DTDelay { starting_timeout_s: 5.0, timeout_s: -1.0 }
+///
+/// println!("Time since ended: {}", delay.time_since_ended()); // Time since ended: 1.0
+/// ```
 
 #[derive(derivative::Derivative, Default, Copy, Debug, Clone, serde::Deserialize)]
 #[derivative(PartialEq)]
 #[serde(from = "f64")]
-/// Mostly used in animations, The Delay is good for waiting (ex: animations frames)
 pub struct DTDelay {
-    starting_timeout_s: f64,
+    starting_timeout_s: f64, // Initial timeout value in seconds
     #[derivative(PartialEq = "ignore")]
-    // i knowingly ignore the emplementation of PartialEq to this field beacause it is so precise that it
-    // is impossible for two timeout to be equal
-    timeout_s: f64, // Set it to the wanted time, (IN SECCONDS) then decrease it with given delta time, when reaches 0, it's done
+    // Ignore this field when comparing two DTDelay instances for equality
+    // because it is too precise to reliably compare.
+    timeout_s: f64, // Current timeout value in seconds; set to the desired time, then decrease with delta time until it reaches 0
 }
 
 impl DTDelay {
+    /// Creates a new `DTDelay` with the specified timeout in seconds.
     pub fn new(timeout_s: f64) -> Self {
         Self {
             timeout_s,
             starting_timeout_s: timeout_s,
         }
     }
-    pub fn new_custom_timeline(timeout_s: f64, offset: f64) -> Self {
-        Self {
-            timeout_s: timeout_s - offset,
-            starting_timeout_s: timeout_s,
-        }
-    }
+
+    /// Restarts the `DTDelay`, resetting the timeout to the initial value.
     pub fn restart(&mut self) {
         *self = Self::new(self.starting_timeout_s)
     }
+
+    /// Restart the `DTDelay` with a custom timeline.
+    /// This is useful for better consistency over low framerate.
+    ///
+    /// # Arguments
+    ///
+    /// * `offset` - The offset to subtract from the timeout value.
+    pub fn restart_custom_timeline(&mut self, offset: f64) {
+        *self = Self {
+            starting_timeout_s: self.timeout_s,
+            timeout_s: self.timeout_s - offset,
+        }
+    }
+
+    /// Updates the timeout by decreasing it with the given delta time.
+    /// # Note
+    ///
+    /// The delta time has to be in seconds
+    ///
+    /// # Arguments
+    ///
+    /// * `dt` - The delta time to subtract from the timeout.
     pub fn update(&mut self, dt: f64) {
         self.timeout_s -= dt;
     }
+
+    /// Returns the fraction of time remaining (0.0 < fraction < 1.0).
     pub fn fraction(&self) -> f64 {
-        // has to be 0.0<frac<1.0
         self.timeout_s / self.starting_timeout_s
     }
+
+    /// Returns true if the timeout has ended (i.e., is less than or equal to 0).
     pub fn ended(&self) -> bool {
         self.timeout_s <= 0f64
     }
+
+    /// Returns the time since the timeout ended.
+    /// If negative, the delay has not finished yet.
+    ///
+    /// This is generally used as offset for [`DTDelay::restart_custom_timeline`]
     pub fn time_since_ended(&self) -> f64 {
-        self.timeout_s * -1. // if this is negative, the delay is not finished yet
+        self.timeout_s * -1.0
     }
 }
 
 impl From<f64> for DTDelay {
+    /// Allows creating a `DTDelay` instance from a `f64` value.
+    ///
+    /// # Arguments
+    ///
+    /// * `timeout_s` - The initial timeout value in seconds.
+    ///
+    /// # Returns
+    ///
+    /// A `DTDelay` instance with the given timeout.
     fn from(timeout_s: f64) -> DTDelay {
         DTDelay::new(timeout_s)
     }
 }
 
+/// Like a std::time::Instant, but you can stop it to read later
+///
+/// # Examples
+///
+/// ```
+/// use time::Stopwatch;
+///
+/// let mut stopwatch = Stopwatch::start_new();
+///
+/// std::thread::sleep(std::time::Duration::from_secs_f32(1.5));
+/// 
+/// println!("{}", stopwatch.read()) // 1.5s
+/// 
+/// stopwatch.stop();
+/// 
+/// std::thread::sleep(std::time::Duration::from_secs(1));
+/// 
+/// println!("{}", stopwatch.read()) // 1.5s
+/// ```
+
 #[derive(Debug, Clone)]
 /// Measure the time between the .start and .stop functions, can be read later
 pub enum Stopwatch {
-    // Ps i used an enum as it best fits the use to me, + it's globally smaller as it re-uses the memory if the other state for the curent one
     Running {
         start_time: std::time::Instant,
     },
@@ -90,12 +163,11 @@ impl Stopwatch {
     pub fn is_running(&self) -> bool {
         matches![self, Stopwatch::Running { .. }]
     }
-    pub fn is_stopped(&self) -> bool {
-        !self.is_running()
-    }
+
     pub fn start(&mut self) {
         *self = Stopwatch::start_new();
     }
+
     pub fn stop(&mut self) {
         if let Self::Running { start_time } = self {
             *self = Stopwatch::Paused {
@@ -104,6 +176,7 @@ impl Stopwatch {
             }
         }
     }
+
     pub fn read(&self) -> std::time::Duration {
         match self {
             Stopwatch::Running { start_time } => start_time.elapsed(),
@@ -118,18 +191,58 @@ impl std::fmt::Display for Stopwatch {
     }
 }
 
-/// The precision is the number of units you want to see displayed
-/// For example:
-///     Example:
-///         time elapsed was 2h 14m 3s 169ms rtc..
+/// Formats a `std::time::Duration` into a human-readable string with specified precision.
 ///
-///         format(1) would give 2h
-///         format(2) would give 2h 14m
+/// # Parameters
 ///
-///         Etcc and ofc it wouldn't show 0h or 0m
+/// - `duration`: The `std::time::Duration` to format.
+/// - `prec`: The precision specifying the number of time units to display.
+///   - If `prec` is a positive integer, it limits the output to that many units, starting from the largest unit (e.g., years, weeks, days, etc.).
+///   - If `prec` is -1, all non-zero units will be displayed.
 ///
-///     Specify -1 to display every units (where the value isn't 0)
+/// # Returns
 ///
+/// A `String` representing the formatted duration with the specified precision.
+///
+/// # Examples
+///
+/// ```
+/// let duration = std::time::Duration::new(80000, 0); // 80,000 seconds
+///
+/// // Displaying only the largest unit (precision = 1)
+/// assert_eq!(time::format(duration, 1), "22h");
+///
+/// // Displaying the two largest units (precision = 2)
+/// assert_eq!(time::format(duration, 2), "22h 13m");
+///
+/// // Displaying all non-zero units (precision = -1)
+/// assert_eq!(time::format(duration, -1), "22h 13m 20s");
+///
+/// // Duration of 0 seconds
+/// let duration_zero = std::time::Duration::new(0, 0);
+/// assert_eq!(time::format(duration_zero, -1), "0ns");
+/// ```
+///
+/// # Notes
+///
+/// - The function handles up to years, weeks, days, hours, minutes, seconds, milliseconds, microseconds, and nanoseconds.
+/// - Units with zero value are omitted in the output.
+/// - The precision parameter affects the number of units shown, starting from the largest available unit.
+///
+/// # Units Conversion
+///
+/// - 1 year = 365 days
+/// - 1 week = 7 days
+/// - 1 day = 24 hours
+/// - 1 hour = 60 minutes
+/// - 1 minute = 60 seconds
+/// - 1 second = 1,000 milliseconds
+/// - 1 millisecond = 1,000 microseconds
+/// - 1 microsecond = 1,000 nanoseconds
+///
+/// # Panics
+///
+/// This function does not panic.
 pub fn format(duration: std::time::Duration, mut prec: i8) -> String {
     const NANOS_IN_MICROSECOND: f64 = 1_000.0;
     const NANOS_IN_MILLISECOND: f64 = 1_000_000.0;
@@ -143,7 +256,7 @@ pub fn format(duration: std::time::Duration, mut prec: i8) -> String {
     let total_nanos = duration.as_nanos() as f64;
 
     if total_nanos < 1.0 {
-        return format!("{:.0}ns", total_nanos);
+        return format!("{:.0}ns", total_nanos.floor());
     }
 
     let mut remaining_nanos = total_nanos;
@@ -152,79 +265,80 @@ pub fn format(duration: std::time::Duration, mut prec: i8) -> String {
     if remaining_nanos >= NANOS_IN_YEAR && prec != 0 {
         prec -= 1;
         let years = remaining_nanos / NANOS_IN_YEAR;
-        formatted_duration.push_str(&format!("{:.0}y ", years));
+        formatted_duration.push_str(&format!("{:.0}y ", years.floor()));
         remaining_nanos %= NANOS_IN_YEAR;
     }
 
     if remaining_nanos >= NANOS_IN_WEEK && prec != 0 {
         prec -= 1;
         let weeks = remaining_nanos / NANOS_IN_WEEK;
-        formatted_duration.push_str(&format!("{:.0}w ", weeks));
+        formatted_duration.push_str(&format!("{:.0}w ", weeks.floor()));
         remaining_nanos %= NANOS_IN_WEEK;
     }
 
     if remaining_nanos >= NANOS_IN_DAY && prec != 0 {
         prec -= 1;
         let days = remaining_nanos / NANOS_IN_DAY;
-        formatted_duration.push_str(&format!("{:.0}d ", days));
+        formatted_duration.push_str(&format!("{:.0}d ", days.floor()));
         remaining_nanos %= NANOS_IN_DAY;
     }
 
     if remaining_nanos >= NANOS_IN_HOUR && prec != 0 {
         prec -= 1;
         let hours = remaining_nanos / NANOS_IN_HOUR;
-        formatted_duration.push_str(&format!("{:.0}h ", hours));
+        formatted_duration.push_str(&format!("{:.0}h ", hours.floor()));
         remaining_nanos %= NANOS_IN_HOUR;
     }
 
     if remaining_nanos >= NANOS_IN_MINUTE && prec != 0 {
         prec -= 1;
         let minutes = remaining_nanos / NANOS_IN_MINUTE;
-        formatted_duration.push_str(&format!("{:.0}m ", minutes));
+        formatted_duration.push_str(&format!("{:.0}m ", minutes.floor()));
         remaining_nanos %= NANOS_IN_MINUTE;
     }
 
     if remaining_nanos >= NANOS_IN_SECOND && prec != 0 {
         prec -= 1;
         let seconds = remaining_nanos / NANOS_IN_SECOND;
-        formatted_duration.push_str(&format!("{:.0}s ", seconds));
+        formatted_duration.push_str(&format!("{:.0}s ", seconds.floor()));
         remaining_nanos %= NANOS_IN_SECOND;
     }
 
     if remaining_nanos >= NANOS_IN_MILLISECOND && prec != 0 {
         prec -= 1;
         let milis = remaining_nanos / NANOS_IN_MILLISECOND;
-        formatted_duration.push_str(&format!("{:.0}ms ", milis));
+        formatted_duration.push_str(&format!("{:.0}ms ", milis.floor()));
         remaining_nanos %= NANOS_IN_MILLISECOND;
     }
 
     if remaining_nanos >= NANOS_IN_MICROSECOND && prec != 0 {
         prec -= 1;
         let micro = remaining_nanos / NANOS_IN_MICROSECOND;
-        formatted_duration.push_str(&format!("{:.0}µs ", micro));
+        formatted_duration.push_str(&format!("{:.0}µs ", micro.floor()));
         remaining_nanos %= NANOS_IN_MICROSECOND;
     }
 
     if remaining_nanos > 0.0 && prec != 0 {
-        formatted_duration.push_str(&format!("{:.0}ns", remaining_nanos));
+        formatted_duration.push_str(&format!("{:.0}ns", remaining_nanos.floor()));
     }
 
     formatted_duration.trim().to_string()
 }
 
+/// Used to time the execution of a function with immutable parameters
+/// # Example
+/// ```
+/// use time::timeit;
+///
+/// fn my_function(s: &str){
+///     std::thread::sleep(std::time::Duration::from_secs(2));
+///     println!("{s}");
+/// }
+///
+/// let s = "Hi";
+/// let (output, duration) = timeit( || my_function(s));
+/// ```
 pub fn timeit<F: FnOnce() -> T, T>(f: F) -> (T, std::time::Duration) {
-    //! Used to time the execution of a function with immutable parameters
-    //! # Example
-    //! ```
-    //! use time::timeit;
-    //!
-    //! fn my_function(){
-    //!     std::thread::sleep(std::time::Duration::from_secs(2));
-    //! }
-    //!
-    //! let (output, duration) = timeit( || my_function() );
-    //! ```
-
     // The order of the output is important as it's also the order that it's computed
     // if you output (start.elapsed(), f()), the timer is stopped before the function actually starts
     // you'll need to compute f() before and store it in an ouput variable
@@ -234,45 +348,43 @@ pub fn timeit<F: FnOnce() -> T, T>(f: F) -> (T, std::time::Duration) {
     (f(), start.elapsed())
 }
 
+/// Used to time the execution of a function with mutable parameters
+/// # Example
+/// ```
+/// use time::timeit_mut;
+///
+/// fn my_mut_function(x: &mut i32){
+///     std::thread::sleep(std::time::Duration::from_secs(2));
+///     *x += 1
+/// }
+///
+/// let mut y = 5;
+/// let (output, duration) = timeit_mut( || my_mut_function(&mut y) );
+/// ```
 pub fn timeit_mut<F: FnMut() -> T, T>(mut f: F) -> (T, std::time::Duration) {
-    //! Used to time the execution of a function with mutable parameters
-    //! # Example
-    //! ```
-    //! use time::timeit_mut;
-    //!
-    //! fn my_mut_function(x: &mut i32){
-    //!     std::thread::sleep(std::time::Duration::from_secs(2));
-    //!     *x += 1
-    //! }
-    //!
-    //! let mut y = 5;
-    //! let (output, duration) = timeit_mut( || my_mut_function(&mut y) );
-    //! ```
-
     let start = std::time::Instant::now();
     // let output = f();
     (f(), start.elapsed())
 }
 
+/// Used to time the execution of a function with mutable parameters
+/// # Example
+/// ```
+/// use time::timeit_async;
+///
+/// async fn my_async_function() -> bool{
+///     std::thread::sleep(std::time::Duration::from_secs(2));
+///     true
+/// }
+///
+/// async fn _main(){
+///     let (output, duration) = timeit_async( || my_async_function() ).await;
+/// }
+///
+/// ```
 pub async fn timeit_async<F: std::future::Future<Output = T>, T>(
     f: impl FnOnce() -> F,
 ) -> (T, std::time::Duration) {
-    //! Used to time the execution of a function with mutable parameters
-    //! # Example
-    //! ```
-    //! use time::timeit_async;
-    //!
-    //! async fn my_async_function() -> bool{
-    //!     std::thread::sleep(std::time::Duration::from_secs(2));
-    //!     true
-    //! }
-    //!
-    //! async fn _main(){
-    //!     let (output, duration) = timeit_async( || my_async_function() ).await;
-    //! }
-    //!
-    //! ```
-
     let start = std::time::Instant::now();
     // let output = f().await;
     (f().await, start.elapsed())
--- time/tests/display.rs
+++ time/tests/display.rs
@@ -1,27 +1,41 @@
-
 #[test]
 fn display() {
     use std::time::Duration;
 
-    let one_nano = Duration::from_secs_f64(0.000000001001);
-    let one_micro = Duration::from_secs_f64(0.000001001);
-    let one_milli = Duration::from_secs_f64(0.001001);
-    let one_sec = Duration::from_secs_f64(1.1);
-    let one_minute = Duration::from_secs_f64(61.);
-    let one_hour = Duration::from_secs(3661);
-    let one_day = Duration::from_secs(90000);
+    let one_nano = Duration::from_secs_f64(0.000000001);
+    let one_micro = Duration::from_secs_f64(0.000001);
+    let one_milli = Duration::from_secs_f64(0.001);
+    let one_sec = Duration::from_secs(1);
+    let one_minute = Duration::from_secs(60);
+    let one_hour = Duration::from_secs(3600);
+    let one_day = Duration::from_secs(86400);
 
-    println!("{}", time::format(one_nano, -1));
-    println!("{}", time::format(one_micro, -1));
-    println!("{}", time::format(one_milli, -1));
-    println!("{}", time::format(one_sec, -1));
-    println!("{}", time::format(one_minute, -1));
-    println!("{}", time::format(one_hour, -1));
-    println!("{}", time::format(one_day, -1));
+    println!("one_nano: {}", time::format(one_nano, -1));
+    // one_nano: 1ns
+    println!("one_micro: {}", time::format(one_micro, -1));
+    // one_micro: 1µs
+    println!("one_milli: {}", time::format(one_milli, -1));
+    // one_milli: 1ms
+    println!("one_sec: {}", time::format(one_sec, -1));
+    // one_sec: 1s
+    println!("one_minute: {}", time::format(one_minute, -1));
+    // one_minute: 1m
+    println!("one_hour: {}", time::format(one_hour, -1));
+    // one_hour: 1h
+    println!("one_day: {}", time::format(one_day, -1));
+    // one_day: 1d
+    println!(
+        "Everything above: {}",
+        time::format(
+            one_nano + one_micro + one_milli + one_sec + one_minute + one_hour + one_day,
+            -1
+        )
+    );
+    // Everything above: 1d 1h 1m 1s 1ms 1µs 1ns
+
+    println!("");
 
 
     println!("{}", time::format(Duration::from_secs(3661), -1));
     println!("{}", time::format(Duration::from_secs(31546321), -1));
-
-
 }
