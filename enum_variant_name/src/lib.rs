2e15876a611f94f59d6e277960f02e3596816583
Bowarc <yuyeaaaahhhh@laposte.net>
2024-11-30 20:58:35 +0100

    Code cleanup

--- enum_variant_name/src/lib.rs
+++ enum_variant_name/src/lib.rs
@@ -24,42 +24,36 @@ use syn::{parse_macro_input, Data, DeriveInput, Error};
 pub fn variant_name(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let name = &input.ident;
-    let data = &input.data;
+    let Data::Enum(enum_data) = &input.data else {
+        return Error::new_spanned(&input, "VariantName can only be implemented for enums")
+            .to_compile_error()
+            .into();
+    };
 
-    let match_arms = match data {
-        Data::Enum(enum_data) => {
-            let match_arms = enum_data.variants.iter().map(|variant| {
-                let variant_ident = &variant.ident;
-                let variant_string = variant_ident.to_string();
-                let fields_in_variant = quote! { {..} };
-                quote! {
-                    Self::#variant_ident #fields_in_variant => #variant_string,
-                }
-            });
-
-            quote! {
-                match self {
-                    #(#match_arms)*
-                }
-            }
+    let match_arms = enum_data.variants.iter().map(|variant| {
+        let variant_ident = &variant.ident;
+        let variant_string = &variant_ident.to_string();
+        let fields_in_variant = quote! { {..} };
+        quote! {
+            Self::#variant_ident #fields_in_variant => #variant_string,
         }
-        _ => {
-            return Error::new_spanned(&input, "VariantName can only be implemented for enums")
-                .to_compile_error()
-                .into();
+    });
+
+    let match_stmnt = quote! {
+        match self {
+            #(#match_arms)*
         }
     };
+
+    let name = &input.ident;
 
     let (impl_generics, ty_generics, where_clause) = &input.generics.split_for_impl();
 
-    let expanded = quote! {
+    TokenStream::from(quote! {
         impl #impl_generics #name #ty_generics #where_clause {
             pub fn variant_name(&self) -> &'static str {
-                #match_arms
-            }
+                #match_stmnt
+           }
         }
-    };
-
-    TokenStream::from(expanded)
+    })
 }
