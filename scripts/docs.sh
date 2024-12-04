9bcd3b2d017a1092a0efb31b47f82e2988795434
Bowarc <yuyeaaaahhhh@laposte.net>
2024-12-04 10:24:00 +0100

    docs script

---
+++ scripts/docs.sh
@@ -0,0 +1,23 @@
+# builds and publish the docs
+
+echo Cleaning old docs
+cargo clean --doc
+rm -rf ./docs
+
+echo Building updated docs
+cargo doc --no-deps
+
+echo Creating ./docs
+mkdir ./docs
+
+echo Copying generated docs to ./docs
+cp -r ./target/doc/* ./docs
+
+echo Commiting docs
+
+git add ./docs
+
+git commit -m "Updating docs"
+
+echo Pushing docs
+git push
