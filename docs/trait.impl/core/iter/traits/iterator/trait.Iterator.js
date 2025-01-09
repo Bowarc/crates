f1c858f247b009a32aaf7fd78a4088cf23e292bb
Bowarc <yuyeaaaahhhh@laposte.net>
2025-01-09 19:18:13 +0100

    Updating docs

--- docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ docs/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["math",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"math/vec2d/struct.Vec2DIterator.html\" title=\"struct math::vec2d::Vec2DIterator\">Vec2DIterator</a>"]]]]);
+    var implementors = Object.fromEntries([["math",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"math/vec2d/struct.Vec2DIterator.html\" title=\"struct math::vec2d::Vec2DIterator\">Vec2DIterator</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- docs/trait.impl/core/ops/drop/trait.Drop.js
+++ docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["networking",[["impl&lt;R: <a class=\"trait\" href=\"networking/message/trait.Message.html\" title=\"trait networking::message::Message\">Message</a>, W: <a class=\"trait\" href=\"networking/message/trait.Message.html\" title=\"trait networking::message::Message\">Message</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"networking/socket/struct.Socket.html\" title=\"struct networking::socket::Socket\">Socket</a>&lt;R, W&gt;"]]]]);
+    var implementors = Object.fromEntries([["networking",[["impl&lt;R: <a class=\"trait\" href=\"networking/message/trait.Message.html\" title=\"trait networking::message::Message\">Message</a>, W: <a class=\"trait\" href=\"networking/message/trait.Message.html\" title=\"trait networking::message::Message\">Message</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"networking/socket/struct.Socket.html\" title=\"struct networking::socket::Socket\">Socket</a>&lt;R, W&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
